87084102497f71530741bb250705b7c546ea323c
Sergei Solodzhuk <solodzh@gmail.com>
2021-09-16 01:05:36 +0200

    9

--- src/modules/formula.js
+++ src/modules/formula.js
@@ -1,11 +1,35 @@
 const formula = () => {
   const formula = document.getElementById('formula');
-  const formulaItem = formula.querySelectorAll('.formula-item');
-  console.log('formula: ', formulaItem);
+  const formulaItem = formula.querySelectorAll('.formula-item__icon');
+  const frame = document.querySelector('.formula-item-popup:before');
+  console.log('frame: ', frame);
+
+  formula.addEventListener('click', e => console.log(e.target));
   formulaItem.forEach(elem => {
-    const description = elem.firstElementChild.firstElementChild.style;
-    description.visibility = 'visible';
-    description.opacity = 1;
+    let element;
+    let description;
+    let frame;
+    console.dir(elem);
+
+    if (elem.classList.contains('mobile-hide')) {
+      element = elem;
+      description = element.firstElementChild.style;
+    } else {
+      element = elem.parentElement;
+      description = element.firstElementChild.firstElementChild.style;
+    }
+    // description.classList.toggle('formula-item-popup-reverse');
+
+    element.addEventListener('mouseenter', e => {
+      description.visibility = 'visible';
+      description.opacity = 1;
+      elem.firstElementChild.classList.toggle('formula-item-popup-reverse');
+    });
+    element.addEventListener('mouseleave1', e => {
+      console.log(e.target);
+      description.visibility = 'hidden';
+      description.opacity = 0.1;
+    });
   });
 
 };
